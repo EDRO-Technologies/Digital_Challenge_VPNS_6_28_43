441e3ff9e8ba561aac59860a822306ae9c3bab78
Barss916 <ilya.surgutskij11@mail.ru>
2025-11-23 02:30:33 +0500

    settings update

--- SmartSchedule/settings.py
+++ SmartSchedule/settings.py
@@ -109,13 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Europe/Yekaterinburg'
 USE_I18N = True
-
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
