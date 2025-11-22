2b137348adebf1066020c722d9bb153e1b615de5
ilnar <hooloit147@list.ru>
2025-11-22 14:30:01 +0300

    add html page profile and auth

--- account/views.py
+++ account/views.py
@@ -3,3 +3,6 @@ from django.views.generic import CreateView, FormView, TemplateView
 
 class Zaglushka(TemplateView):
     template_name = "authorization.html"
+
+class Zaglushka2(TemplateView):
+    template_name = "profile.html"
