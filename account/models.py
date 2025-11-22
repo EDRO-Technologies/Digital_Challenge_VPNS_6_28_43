6e1ed14f5536ab3719fa692773674954e124e4db
Barss916 <ilya.surgutskij11@mail.ru>
2025-11-22 15:57:38 +0500

    database + urls

--- account/models.py
+++ account/models.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.models import User
-from django.models import models
+from django.db import models
 
 
 class Profile(models.Model):
+    class Roles(models.TextChoices):
+        STUDENT = "ST"
+        TEACHER = "T"
+        OTHER = "O"
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
+    last_name = models.CharField(name="last name", max_length=75)
+    name = models.CharField(name="name", max_length=75)
+    surname = models.CharField(name="surname", max_length=75)
+    phone = models.CharField(name="phone", max_length=20)
+    role = models.CharField(name="role", choices=Roles, default=Roles.STUDENT)
+
+    def __str__(self):
+        return self.email
+
+class StudentsCourse(models.Model):
+    title = models.CharField(name="student course title", max_length=30)
+    participants = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    institute = models.CharField(name="institute", max_length=5)
+
+    def __str__(self):
+        return self.title
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from account.models import StudentsCourse
 
-# Create your models here.
+class Course(models.Model):
+    title = models.TextField(name="course title")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(name="course description")
+
+    def __str__(self):
+        return title
+
+class Lessons(models.Model):
+    title = models.TextField(name="lesson title") 
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(name="lesson date")
+
+class Notifications_lessons(models.Model):
+    title = models.CharField(name="notififcation title", max_length=255)
+    lesson_title = models.ForeignKey(Lessons, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(name="notification description")
+
+class Notifications_courses(models.Model):
+    title = models.CharField(name="notififcation title", max_length=255)
+    course_title = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(name="notification description")
+
+class StudyGroup(models.Model):
+    participants = models.ManyToManyField(StudentsCourse)
+    lessons = models.ForeignKey(Lessons, on_delete=models.CASCADE, name="lessons")
+
